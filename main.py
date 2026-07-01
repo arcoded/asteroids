251fbe3af3b585cf26a1f3a94007c4c5a2e33b2d
arcoded <aayanrizwan1013@gmail.com>
2026-07-01 02:02:37 -0400

    Sprite added with hitbox.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0.0
+    player_one = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,7 +17,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_one.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
