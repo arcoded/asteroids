d71eb24a0582959c06d1ce66f9bf238e32c9c28e
arcoded <aayanrizwan1013@gmail.com>
2026-07-04 02:08:10 -0400

    Did some light refactoring and added the asteroids to the screen albeit with no collision.

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+
 def main():
     pygame.init()
     game_clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player_one = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,8 +27,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player_one.draw(screen)
-        player_one.update(dt)
+        for group_object in drawable:
+            group_object.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
